dd99adf5eafdd50c16f05171bae247cfa6e8bc5c
Smakuwa <45312377+Smakuwa@users.noreply.github.com>
2021-03-24 01:11:14 +0100

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,19 @@
+/*
+    This is an example of a credentials file that can be used to store your
+    credentials object (see docs), which can be imported as a JavaScript module
+    at runtime and passed directly to the login function like this:
+    ```js
+    const botcore = require("messenger-botcore");
+    const credentials = require("credentials.js");
+    botcore.login.login(credentials, (err, api) => {
+        // ...
+    });
+    ```
+    REMEMBER TO ADD THIS FILE TO YOUR .GITIGNORE!!!
+*/
+
+exports.MEMCACHIER_USERNAME = "03034C";
+exports.MEMCACHIER_PASSWORD = "E1F9EA58DE8EDFA2BED93C6DC8318BE8";
+exports.MEMCACHIER_SERVERS = "mc1.dev.eu.ec2.memcachier.com:11211";
+exports.FACEBOOK_EMAIL = "smakubot@gmail.com";
+exports.FACEBOOK_PASSWORD = "gmd2137gmd";
